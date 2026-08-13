c20148608c5610ca7b1938c2ce2eccc9c544497a
Sam <itsmeitself7777@gmail.com>
2026-08-13 19:00:44 +0530

    Enhance StringDemo functionality by adding string comparison examples. Updated StringDemo.java to include equality checks using '==' and 'equals' methods. Binary file StringDemo.class updated accordingly.

--- StringDemo.java
+++ StringDemo.java
@@ -3,9 +3,13 @@ public class StringDemo {
     public static void main(String args[]) {
 
         String s1 = "hello";
+        String s2 = "hello";
         String s3 = new String("hello");
 
+        System.out.println(s1 == s2);
         System.out.println(s1 == s3);
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
 
     }
 }
