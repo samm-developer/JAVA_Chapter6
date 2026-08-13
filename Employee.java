1e739c3b74d4dfdfb13f2b8c793af67bc7505934
Sam <itsmeitself7777@gmail.com>
2026-08-13 18:57:31 +0530

    Refactor Employee and Student classes by removing unnecessary whitespace and ensuring consistent formatting. Updated Employee.java and Student.java files. Binary files Employee.class and Student.class also updated.

--- Employee.java
+++ Employee.java
@@ -1,12 +1,9 @@
 public class Employee {
     int empId;
-
     public int getEmpId() {
         return empId;
     }
-
     public void setEmpId(int empId) {
         this.empId = empId;
     }
 }
- 
--- Student.java
+++ Student.java
@@ -1,13 +1,10 @@
 public class Student {
-
     public static void main(String args[]) {
-
         Employee empObject = new Employee();
         empObject.empId = 10;
         modify(empObject);
         System.out.println(empObject.empId);
     }
-
     private static void modify(Employee employee) {
         employee.empId = 20;
     }
